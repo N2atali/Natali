a71e57cb95843f262ff4fdf56f40be11d9c661a2
N2atali <natali.cat2807@gmail.com>
2023-08-12 19:04:06 +0300

    Fixed

---
+++ src/homework1/Task5.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class Task5
+{
+    public static void main (String [] args)
+    {
+        int q = 21;
+        int w = 8;
+        int a = q/w;
+        int b = q%w;
+
+        System.out.println(a + " и " + b + " в остатке ");
+    }
+}
